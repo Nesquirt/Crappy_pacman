c13e40127683125b9420ea1e0254c285c259eb94
Nesquirt <sg5027@gmail.com>
2024-01-08 17:57:45 +0100

    lil fix

--- Ghost.java
+++ Ghost.java
@@ -1,78 +1,34 @@
+import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
 public class Ghost {
-    private String name;
     private int x;
     private int y;
+    private int size;
+    private String name;
     private int speed;
-    private MazeTemplate mazeTemplate;
+    private int direction;
     private Pacman pacman;
+    private MazeTemplate mazeTemplate;
     private Random random;
+    private int changeDirectionProbability; // Probabilità di cambiare direzione (in percentuale)
 
-    public Ghost(String name, int startX, int startY, MazeTemplate mazeTemplate, Pacman pacman, int speed) {
+    public Ghost(String name, int x, int y, MazeTemplate mazeTemplate, Pacman pacman, int speed) {
+        this(name, x, y, mazeTemplate, pacman, speed, 0); // Imposta la probabilità di cambiare direzione a 0% di default
+    }
+
+    public Ghost(String name, int x, int y, MazeTemplate mazeTemplate, Pacman pacman, int speed, int changeDirectionProbability) {
         this.name = name;
-        this.x = startX;
-        this.y = startY;
-        this.mazeTemplate = mazeTemplate;
-        this.pacman = pacman;
+        this.x = x;
+        this.y = y;
+        this.size = 20;
         this.speed = speed;
+        this.pacman = pacman;
+        this.direction = 0; // Inizia muovendosi verso l'alto
+        this.mazeTemplate = mazeTemplate;
         this.random = new Random();
-    }
-
-    public void move() {
-        // Implementa la logica di movimento del fantasma
-        // Ad esempio, puoi farli inseguire il giocatore o muoversi in modo casuale
-
-        int nextX = x;
-        int nextY = y;
-
-        // Scegli una direzione in base alla posizione del giocatore
-        int playerX = pacman.getX();
-        int playerY = pacman.getY();
-
-        if (random.nextBoolean()) {
-            moveHorizontally(playerX);
-        } else {
-            moveVertically(playerY);
-        }
-    }
-
-    private void moveHorizontally(int playerX) {
-        // Muovi in direzione orizzontale
-        if (playerX < x && isValidMove(x - speed, y)) {
-            x -= speed;
-        } else if (playerX > x && isValidMove(x + speed, y)) {
-            x += speed;
-        }
-    }
-
-    private void moveVertically(int playerY) {
-        // Muovi in direzione verticale
-        if (playerY < y && isValidMove(x, y - speed)) {
-            y -= speed;
-        } else if (playerY > y && isValidMove(x, y + speed)) {
-            y += speed;
-        }
-    }
-
-    private boolean isValidMove(int x, int y) {
-        // Verifica se la mossa è valida
-        int cellX = x / mazeTemplate.CELL;
-        int cellY = y / mazeTemplate.CELL;
-
-        if (cellX >= 0 && cellX < mazeTemplate.getColumnCount() && cellY >= 0 && cellY < mazeTemplate.getRowCount()) {
-            char cellType = mazeTemplate.getMazeData()[cellY][cellX];
-            return cellType != 'x' && cellType != 'v' && cellType != 'h' && cellType != '1' && cellType != '2' && cellType != '3' && cellType != '4';
-        }
-
-        return false;
-    }
-
-    public void draw(Graphics g) {
-        // Implementa la logica di disegno del fantasma
-        g.setColor(Color.RED); // Cambia colore a seconda del fantasma
-        g.fillOval(x - 10, y - 10, 20, 20); // Disegna un cerchio per rappresentare il fantasma
+        this.changeDirectionProbability = changeDirectionProbability;
     }
 
     public int getX() {
@@ -83,13 +39,92 @@ public class Ghost {
         return y;
     }
 
+    public int getSize() {
+        return size;
+    }
+
     public String getName() {
-        // Restituisci il nome del fantasma
         return name;
     }
 
-    public int getSize() {
-        // Restituisci la dimensione del fantasma
-        return 20;
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void move() {
+        if (pacman.isMoving()) {
+            moveTowardsPacman();
+            randomlyChangeDirection();
+        } else {
+            // Se Pac-Man è fermo, continua nella direzione attuale
+            continueInCurrentDirection();
+        }
+    }
+
+    private void moveTowardsPacman() {
+        int targetX = pacman.getX();
+        int targetY = pacman.getY();
+
+        int deltaX = targetX - getX();
+        int deltaY = targetY - getY();
+
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            moveHorizontally(deltaX);
+        } else {
+            moveVertically(deltaY);
+        }
+    }
+
+    private void moveHorizontally(int deltaX) {
+        int step = Integer.compare(deltaX, 0);
+        int nextX = getX() + step * mazeTemplate.CELL;
+
+        if (isValidMove(nextX, getY())) {
+            setX(nextX);
+        }
+    }
+
+    private void moveVertically(int deltaY) {
+        int step = Integer.compare(deltaY, 0);
+        int nextY = getY() + step * mazeTemplate.CELL;
+
+        if (isValidMove(getX(), nextY)) {
+            setY(nextY);
+        }
+    }
+
+    private void continueInCurrentDirection() {
+        if (direction == 0) {
+            moveVertically(-1); // Sposta verso l'alto
+        } else if (direction == 1) {
+            moveVertically(1); // Sposta verso il basso
+        } else if (direction == 2) {
+            moveHorizontally(-1); // Sposta verso sinistra
+        } else if (direction == 3) {
+            moveHorizontally(1); // Sposta verso destra
+        }
+    }
+
+    private void randomlyChangeDirection() {
+        // Aggiungi una probabilità del 5% di cambiare direzione ad ogni passo
+        if (random.nextDouble() < changeDirectionProbability / 100.0) {
+            direction = random.nextInt(4);
+        }
+    }
+
+    private boolean isValidMove(int x, int y) {
+        int cellX = x / mazeTemplate.CELL;
+        int cellY = y / mazeTemplate.CELL;
+
+        if (cellX >= 0 && cellX < mazeTemplate.getColumnCount() && cellY >= 0 && cellY < mazeTemplate.getRowCount()) {
+            char cellType = mazeTemplate.getMazeData()[cellY][cellX];
+            return cellType != 'x' && cellType != 'v' && cellType != 'h' && cellType != '1' && cellType != '2' && cellType != '3' && cellType != '4';
+        }
+
+        return false;
     }
 }
