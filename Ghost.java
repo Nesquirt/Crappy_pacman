ba3f8075e63bb5314197d620cba81a2b861ffec0
Nesquirt <sg5027@gmail.com>
2024-01-22 15:46:44 +0100

    Fixed some error in compile

--- Ghost.java
+++ Ghost.java
@@ -20,7 +20,7 @@ public class Ghost {
     private Timer moveTimer;
     private long lastMoveTime = System.currentTimeMillis();
     private int millisecondsPerMove = 16; // Regola questo valore per cambiare la velocità
-    private GhostLogic ghostLogic;
+    public GhostLogic ghostLogic;
 
     public Ghost(String name, int x, int y, MazeTemplate mazeTemplate, Pacman pacman, int speed, int changeDirectionProbability) {
         this.name = name;
@@ -60,7 +60,17 @@ public class Ghost {
     public String getName() {
         return name;
     }
+    public Random getRandom() {
+        return random;
+    }
 
+    public int getChangeDirectionProbability() {
+        return changeDirectionProbability;
+    }
+
+    public void setDirection(int newDirection) {
+        this.direction = newDirection;
+    }
     public void setX(int x) {
         this.x = x;
     }
@@ -99,7 +109,7 @@ public class Ghost {
         moveInDirection(newDirection);
     }
 
-    private void moveInDirection(int newDirection) {
+    public void moveInDirection(int newDirection) {
         if (newDirection == 0) {
             moveVertically(-1); // Sposta verso l'alto
         } else if (newDirection == 1) {
